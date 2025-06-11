746a6008260d745a28c7acb6a473f4b0a846b164
betofilippi <209280800+JayZanela@users.noreply.github.com>
2025-06-11 14:38:01 -0300

    Responsividade nos Cards de historico dos movimentos

--- src/pages/movimentacao/MovimentosAntigos.tsx
+++ src/pages/movimentacao/MovimentosAntigos.tsx
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { api } from '../../lib/api';
-import './DropdownCards.css';
-import { format, parseISO } from 'date-fns';
+import React, { useState, useEffect } from "react";
+import { api } from "../../lib/api";
+import "./DropdownCards.css";
+import { format, parseISO } from "date-fns";
 
-import { Button } from '../../components/ui/Button';
-import { PackagePlus, ArrowRightLeft, ArrowRightCircle, PackageMinus } from 'lucide-react';
+import { Button } from "../../components/ui/Button";
+import {
+  PackagePlus,
+  ArrowRightLeft,
+  ArrowRightCircle,
+  PackageMinus,
+} from "lucide-react";
 
 interface paramMovimentosAntigoPrps {
-    enderecoBusca: string;
+  enderecoBusca: string;
 }
 
 interface Movimento {
@@ -19,137 +24,143 @@ interface Movimento {
   endereco_para: string;
 }
 
+export const MovimentosAntigos: React.FC<paramMovimentosAntigoPrps> = ({
+  enderecoBusca,
+}) => {
+  const [paramBuscaEndereco, setparamBuscaEndereco] = useState(enderecoBusca);
+  const [aberto, setAberto] = useState(false);
+  const [listaMovimentos, setListaMovimentos] = useState<Movimento[]>([]);
+  const [loadingMovimentos, setloadingMovimentos] = useState(true);
+  const [listaFiltradaMovimentos, setlistaFiltradaMovimentos] =
+    useState(listaMovimentos);
 
-export const MovimentosAntigos: React.FC<paramMovimentosAntigoPrps> = ({enderecoBusca}) => {
+  useEffect(() => {
+    console.log("effecct lista");
+    setparamBuscaEndereco(enderecoBusca);
+    requestBuscaDetalhes();
+  }, [enderecoBusca]);
 
-    const [paramBuscaEndereco, setparamBuscaEndereco] = useState(enderecoBusca);
-      const [aberto, setAberto] = useState(false);
-      const [listaMovimentos, setListaMovimentos] = useState<Movimento[]>([]);
-      const [loadingMovimentos, setloadingMovimentos ] = useState(true)
-      const [listaFiltradaMovimentos, setlistaFiltradaMovimentos] = useState(listaMovimentos);
+  useEffect(() => {
+    if (enderecoBusca === "") {
+      setlistaFiltradaMovimentos(listaMovimentos);
+      return;
+    }
 
+    const listaFiltrada = listaMovimentos.filter(
+      (movimento) =>
+        movimento.endereco_de === enderecoBusca ||
+        movimento.endereco_para === enderecoBusca
+    );
 
-    useEffect(() => {
-        console.log('effecct lista');
-        setparamBuscaEndereco(enderecoBusca);
-        requestBuscaDetalhes();
+    setlistaFiltradaMovimentos(listaFiltrada);
+  }, [enderecoBusca, listaMovimentos]);
 
-
-
-    }, [enderecoBusca])
-
-useEffect(() => {
-  if (enderecoBusca === '') {
-    setlistaFiltradaMovimentos(listaMovimentos);
-    return;
-  }
-
-  const listaFiltrada = listaMovimentos.filter(
-    (movimento) =>
-      movimento.endereco_de === enderecoBusca || movimento.endereco_para === enderecoBusca
-  );
-
-  setlistaFiltradaMovimentos(listaFiltrada);
-}, [enderecoBusca, listaMovimentos]);
-
-
-
-    const requestBuscaDetalhes =  async () => {
+  const requestBuscaDetalhes = async () => {
     try {
       setloadingMovimentos(true);
-    const runBuscadetalhes = await api.buscarMovimentosLike( {colunasParam: [],
-  termoParam: ''} );
+      const runBuscadetalhes = await api.buscarMovimentosLike({
+        colunasParam: [],
+        termoParam: "",
+      });
 
-     
-    const movimentos = runBuscadetalhes.map((movimento: any) => ({
-      id: movimento.id,
-      tipo: movimento.tipo,
-      quantidade: movimento.quantidade,
-      dataMovi: parseISO(movimento.data_hora),
-      endereco_de: movimento.ocupacoes_estoque_movimentacoes_ocupacao_origem_idToocupacoes_estoque?.posicoes_estoque_ocupacoes_estoque_posicao_idToposicoes_estoque?.endereco || '',
-      endereco_para:  movimento.ocupacoes_estoque_movimentacoes_ocupacao_destino_idToocupacoes_estoque?.posicoes_estoque_ocupacoes_estoque_posicao_idToposicoes_estoque?.endereco || '',
-    }));
+      const movimentos = runBuscadetalhes.map((movimento: any) => ({
+        id: movimento.id,
+        tipo: movimento.tipo,
+        quantidade: movimento.quantidade,
+        dataMovi: parseISO(movimento.data_hora),
+        endereco_de:
+          movimento
+            .ocupacoes_estoque_movimentacoes_ocupacao_origem_idToocupacoes_estoque
+            ?.posicoes_estoque_ocupacoes_estoque_posicao_idToposicoes_estoque
+            ?.endereco || "",
+        endereco_para:
+          movimento
+            .ocupacoes_estoque_movimentacoes_ocupacao_destino_idToocupacoes_estoque
+            ?.posicoes_estoque_ocupacoes_estoque_posicao_idToposicoes_estoque
+            ?.endereco || "",
+      }));
 
-        if (paramBuscaEndereco) {
-            movimentos
-        }
-    setListaMovimentos(movimentos);
-    setloadingMovimentos(false);
-
+      if (paramBuscaEndereco) {
+        movimentos;
+      }
+      setListaMovimentos(movimentos);
+      setloadingMovimentos(false);
     } catch (error: any) {
-        console.log(error)
+      console.log(error);
     }
-        };
+  };
 
-
-
-
-    return (
-    <div className="dropdown-container max-w-[90%]">
-      {loadingMovimentos ? (<div className='font-bold'> Carregando.. </div>) :       <button onClick={() => setAberto(!aberto)} className="dropdown-button font-bold ">
-        {aberto ? 'Fechar Historico ▲' : 'Ultimas Movimentações ▼'}
-      </button>}
-
+  return (
+    <div className="dropdown-container max-w-[100%]">
+      {loadingMovimentos ? (
+        <div className="font-bold"> Carregando.. </div>
+      ) : (
+        <button
+          onClick={() => setAberto(!aberto)}
+          className="dropdown-button font-bold "
+        >
+          {aberto ? "Fechar Historico ▲" : "Ultimas Movimentações ▼"}
+        </button>
+      )}
 
       {aberto && (
         <div className="dropdown-content">
-
-
-
-
-        {listaFiltradaMovimentos.length === 0 && (
-          <h3>Sem Movimentos parao Endereço Buscado.</h3>
-
-        )}
+          {listaFiltradaMovimentos.length === 0 && (
+            <h3>Sem Movimentos parao Endereço Buscado.</h3>
+          )}
 
           {[...listaFiltradaMovimentos].reverse().map((card) => (
-            <div key={card.id} className={'card text-start'}>
-
-
-
-                <div className='flex'>
-                    <div className='m-2'>                            
-        {card.tipo === 'Entrada' && (<PackagePlus size={35} />)}
-        {card.tipo === 'Saída' && (<PackageMinus size={35} />)}
-        {card.tipo === 'Transferência' && (<ArrowRightLeft size={35} />)}</div>
-        <div className='m-3 text-left w-[100px]'>
-            <h3 className='font-bold'>{card.tipo}</h3>
-        </div>
-        <div className='ml-5 w-[40%]'>
-            <div>
-                <p> Quantidade: {card.quantidade}</p> 
+            <div key={card.id} className={"card text-start"}>
+              <div className="display">
+                <div className="grid grid-cols-2">
+                  <div className="ml-5 m-2">
+                    {card.tipo === "Entrada" && <PackagePlus size={35} />}
+                    {card.tipo === "Saída" && <PackageMinus size={35} />}
+                    {card.tipo === "Transferência" && (
+                      <ArrowRightLeft size={35} />
+                    )}
+                  </div>
+                  <div className=" ml-5 m-3 text-left w-[100px]">
+                    <h3 className="font-bold">{card.tipo}</h3>
+                  </div>
                 </div>
-            <div>
-                <p> Data do movimento: {format(card.dataMovi, 'dd/MM/yyyy HH:mm')}</p> 
+                <div className="grid sm:grid-cols-1 lg:grid-cols-2">
+                  <div className="ml-5">
+                    <div>
+                      <p> Quantidade: {card.quantidade}</p>
+                    </div>
+                    <div>
+                      <p>
+                        {" "}
+                        Data do movimento:{" "}
+                        {format(card.dataMovi, "dd/MM/yyyy HH:mm")}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="ml-5 w-[40%]">
+                    <div>
+                      {card.tipo === "Entrada"
+                        ? "Entrada para"
+                        : card.tipo === "Saída"
+                        ? "Saída de"
+                        : "De -> Para"}
+                    </div>
+                    <div className={`text-gray-600 font-bold `}>
+                      {card.tipo === "Entrada"
+                        ? `${card.endereco_para}`
+                        : card.tipo === "Saída"
+                        ? `${card.endereco_de}`
+                        : `${card.endereco_de} --> ${card.endereco_para}`}
+                    </div>
+                  </div>
                 </div>
-        </div>
-
-            <div className='ml-4 text-center w-[40%]'>
-              <div>
-               {card.tipo === 'Entrada' ? 'Entrada para' : (card.tipo === 'Saída' ? 'Saída de' : 'De -> Para' )}
               </div>
-                <div className={`text-gray-600 font-bold `}>
-
-                  {card.tipo === 'Entrada' ? `${card.endereco_para}` : (card.tipo === 'Saída' ? `${card.endereco_de}` : `${card.endereco_de} --> ${card.endereco_para}`)}
-                </div>
-            </div>    
-
-                </div>
-
-
-
-            
-
-
             </div>
-        ))}
+          ))}
         </div>
       )}
     </div>
   );
-
-
 };
-
 
 export default MovimentosAntigos;
